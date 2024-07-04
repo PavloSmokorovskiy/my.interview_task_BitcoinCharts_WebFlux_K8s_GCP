e6938567f905b0619daf5c2cdafaa87a56a2ac44
Pavlo Smokorovskiy <pavvelsm@gmail.com>
2024-07-04 14:32:38 -0400

    Initial commit: Setup basic Spring Boot structure with a Hello World controller and view

---
+++ src/main/java/com/pavvels/bitcoincharts/BitcoinChartsApplication.java
@@ -0,0 +1,13 @@
+package com.pavvels.bitcoincharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinChartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinChartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pavvels/bitcoincharts/BitcoinChartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavvels.bitcoincharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinChartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
